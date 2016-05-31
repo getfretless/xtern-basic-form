4885f8dbb79a3658cac154dcaf354f6d82c6a156
Alex Overbeck <alexoverbeck@gmail.com>
2016-05-31 11:57:14 -0400

    handle form submission with javascript

--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
-$(document).foundation()
+$(document).foundation();
+
+BasicForm = {
+  init: function() {
+    this.handleFormSubmit();
+  },
+
+  handleFormSubmit: function() {
+    document.querySelector('form').onsubmit = function(event) {
+      var list = BasicForm.buildListItem(event.target);
+      var details = document.querySelector('.details');    
+      details.appendChild(list);
+      event.target.reset();
+      event.preventDefault();
+    }
+  },
+
+  buildListItem: function(form) {
+    var list = document.createElement('dl');
+    list.innerHTML += '<dt>Name:</dt>';
+    list.innerHTML += '<dd>' + form.name.value + '</dd>';
+    list.innerHTML += '<dt>Hair Color:</dt>';
+    list.innerHTML += '<dd>' + form.color.value + '</dd>';
+    list.innerHTML += '<dt>Age:</dt>';
+    list.innerHTML += '<dd>' + form.age.value + '</dd>';
+    list.innerHTML += '<dt>Birthplace:</dt>';
+    list.innerHTML += '<dd>' + form.birthplace.value + '</dd>';
+    return list;
+  }
+}
+
+BasicForm.init();
